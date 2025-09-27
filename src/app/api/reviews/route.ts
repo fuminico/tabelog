1898a6db06f959616de7f21d61de722277e41498
fuminico <fuminico@gmail.com>
2025-09-27 11:46:09 +0900

    refactor: Implement Prisma singleton for Vercel

--- src/app/api/reviews/route.ts
+++ src/app/api/reviews/route.ts
@@ -1,7 +1,6 @@
-import { PrismaClient } from '@prisma/client';
+import { prisma } from '@/lib/prisma';
 import { NextResponse, NextRequest } from 'next/server';
 
-const prisma = new PrismaClient();
 const postTimestamps = new Map<string, number>();
 const POST_INTERVAL_SECONDS = 60;
 
